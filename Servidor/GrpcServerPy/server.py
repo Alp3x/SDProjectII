557bdd31cf570ab363358c36b539196ea35a2351
Francisco Carvalho <101025751+Alp3x@users.noreply.github.com>
2025-06-09 18:27:37 +0100

    Server

---
+++ Servidor/GrpcServerPy/server.py
@@ -0,0 +1,18 @@
+import grpc
+from concurrent import futures
+import service_pb2
+import service_pb2_grpc
+
+class MyService(service_pb2_grpc.MyServiceServicer):
+    def SayHello(self, request, context):
+        return service_pb2.HelloReply(message=f"Hello, {request.name} from Python!")
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    service_pb2_grpc.add_MyServiceServicer_to_server(MyService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
